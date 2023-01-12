bc24b87f45e8c167f3a6a5d507ddb47ef32e594b
leonfarafonov <117378906+leonfarafonov@users.noreply.github.com>
2023-01-12 09:16:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SpringRestCRUD
+ Crud appliaction created with uses Spring Framework
---
+++ src/test/java/org/crud/restapp/RestappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.crud.restapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
